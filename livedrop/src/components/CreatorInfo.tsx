6881ab15f9bfc51cce44cc0ff808600567f47e1a
MX <mxber2022@gmail.com>
2025-07-02 21:01:10 +0700

    Create CreatorInfo.tsx

---
+++ livedrop/src/components/CreatorInfo.tsx
@@ -0,0 +1,17 @@
+function CreatorInfo() {
+  return (
+    <div className="creator-info">
+      <img
+        src="https://placehold.co/80x80"
+        alt="Creator"
+        className="avatar"
+      />
+      <div>
+        <h3>Live with Jane Doe</h3>
+        <p>Building the future of decentralized streaming.</p>
+      </div>
+    </div>
+  );
+}
+
+export default CreatorInfo; 

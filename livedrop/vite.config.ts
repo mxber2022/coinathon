861d9d66c36fe4ac72c5dec76a874dae5ce8d1a8
mxber2022 <mxber2022@gmail.com>
2025-07-08 17:46:08 +0700

    tainwind css

--- livedrop/vite.config.ts
+++ livedrop/vite.config.ts
@@ -1,9 +1,9 @@
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
   server: {
     allowedHosts: true,
   },
